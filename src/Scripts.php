cc1c073b8136e139481f3f5f0770ec4443df2581
Zhmayev Yaroslav <salaros@yandex.ru>
2016-03-06 11:38:16 +0300

    fixed code indentation in src/Scripts.php

--- src/Scripts.php
+++ src/Scripts.php
@@ -25,7 +25,7 @@ class Scripts
         require_once sprintf('%s/autoload.php', self::$vendorDir);
 
         self::$dotEnv = new Dotenv(self::$rootDir);
-		self::$dotEnv->load();
+        self::$dotEnv->load();
 
         self::$wpCli = sprintf('%s/bin/wp', self::$vendorDir);
     }
@@ -34,46 +34,46 @@ class Scripts
     {
         self::init($event);
 
-		self::$dotEnv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD'])->notEmpty();
+        self::$dotEnv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD'])->notEmpty();
 
-		echo shell_exec(sprintf('%s --allow-root db drop --yes', self::$wpCli));
-		echo shell_exec(sprintf('%s --allow-root db create', self::$wpCli));
+        echo shell_exec(sprintf('%s --allow-root db drop --yes', self::$wpCli));
+        echo shell_exec(sprintf('%s --allow-root db create', self::$wpCli));
     }
 
     public static function createTables(Event $event)
     {
         self::init($event);
 
-		self::$dotEnv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD'])->notEmpty();
+        self::$dotEnv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD'])->notEmpty();
 
-		$shellCmd = sprintf('%s --allow-root core install --url=%s --title=%s --admin_user=%s --admin_password=%s --admin_email=%s --skip-email',
-							self::$wpCli,
-							getenv('WP_HOME'),
-							getenv('WP_TITLE'),
-							getenv('WP_ADMIN'),
-							getenv('WP_ADMIN_PASSWORD'),
-							getenv('WP_ADMIN_EMAIL'));
-		echo shell_exec($shellCmd);
+        $shellCmd = sprintf('%s --allow-root core install --url=%s --title=%s --admin_user=%s --admin_password=%s --admin_email=%s --skip-email',
+                            self::$wpCli,
+                            getenv('WP_HOME'),
+                            getenv('WP_TITLE'),
+                            getenv('WP_ADMIN'),
+                            getenv('WP_ADMIN_PASSWORD'),
+                            getenv('WP_ADMIN_EMAIL'));
+        echo shell_exec($shellCmd);
     }
 
     public static function activatePlugins(Event $event)
     {
         self::init($event);
 
-		self::$dotEnv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD'])->notEmpty();
+        self::$dotEnv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD'])->notEmpty();
 
         $pluginDir = sprintf('%s/wp-content/plugins', self::$rootDir);
-		$pluginDirHandle = opendir($pluginDir);
+        $pluginDirHandle = opendir($pluginDir);
 
-		if (empty($pluginDirHandle)) {
-			return; // TODO report: failed to open pugin directory
-		}
+        if (empty($pluginDirHandle)) {
+            return; // TODO report: failed to open pugin directory
+        }
 
-		while (false !== ($entry = readdir($pluginDirHandle))) {
+        while (false !== ($entry = readdir($pluginDirHandle))) {
             $entryPath = sprintf('%s/%s', $pluginDir, $entry);
             if ($entry != '.' && $entry != '..' && is_dir($entryPath)) {
-				$shellCmd = sprintf('%s --allow-root plugin activate %s', self::$wpCli, $entry);
-				echo shell_exec($shellCmd);
+                $shellCmd = sprintf('%s --allow-root plugin activate %s', self::$wpCli, $entry);
+                echo shell_exec($shellCmd);
             }
         }
     }
